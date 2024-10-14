30813e9a611e80d2ebefa6698ddc893f26541649
MN-House4it <magn9814@edu.sde.dk>
2024-10-14 14:29:00 +0200

    OK NAV

--- H3Calc/app/_layout.tsx
+++ H3Calc/app/_layout.tsx
@@ -1,18 +1,17 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import React, { useEffect } from 'react';
 import Calculator from '../components/Calculator';
 import CalculatorOverview from '../components/CalculatorOverview';
-
-
-
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
+
+const Stack = createNativeStackNavigator();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -32,7 +31,12 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-    <CalculatorOverview />
+      <NavigationContainer independent={true}>
+        <Stack.Navigator initialRouteName="CalculatorOverview">
+          <Stack.Screen name="Calculator Overview" component={CalculatorOverview} />
+          <Stack.Screen name="Calculator" component={Calculator} />
+        </Stack.Navigator>  
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
